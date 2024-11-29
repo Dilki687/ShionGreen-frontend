96bc1d86991ff9e4f07357cf5e478cc8250e0c07
Dilki687 <chamodidilki44@gmail.com>
2024-11-29 13:17:04 +0530

    change link2

--- src/screens/OrderForm.js
+++ src/screens/OrderForm.js
@@ -25,7 +25,7 @@ const OrderForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/orders", {
+      const response = await fetch("https://shion-green-backend.vercel.app/api/orders", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
